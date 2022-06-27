d7afdc7aa3e7090ac0c81c21d107c0d0eee3466f
Morteza.AmeliBidar <mortezaameli1372@yahoo.com>
2022-06-27 15:39:13 +0430

    create basic structure of the Task and Prohect Model

--- backend/taskmanager_app/models.py
+++ backend/taskmanager_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=80, unique=True, blank=False, null=False)
+    description = models.TextField(default='', blank=True, null=True)
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256, blank=False, null=False)
+    description = models.TextField(default='', blank=True, null=True)
