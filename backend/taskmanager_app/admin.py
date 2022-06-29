3f49b16107f9ec74a8ddeae2b1996349fd980930
Morteza.AmeliBidar <mortezaameli1372@yahoo.com>
2022-06-29 15:02:47 +0430

    Add some important Model, and register them to admin
    Add some of basic Model class(Project,Membership, Task) with their important fields
    Register these models to admin models

--- backend/taskmanager_app/admin.py
+++ backend/taskmanager_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Project, Task, Membership
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+
+
+class TaskAmdin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'project']
+
+
+class MembershipAmdin(admin.ModelAdmin):
+    list_display = ['project', 'user', 'inviter', 'user_role', 'confirmed']
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Membership, MembershipAmdin)
+admin.site.register(Task, TaskAmdin)
--- backend/taskmanager_app/models.py
+++ backend/taskmanager_app/models.py
@@ -1,12 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Project(models.Model):
     name = models.CharField(max_length=80, unique=True, blank=False, null=False)
-    description = models.TextField(default='', blank=True, null=True)
+    members = models.ManyToManyField(
+            User,
+            through='Membership',
+            through_fields=('project', 'user'),
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Membership(models.Model):
+    ADMIN_ROLE = 'Admin'
+    USER_ROLE = 'User'
+    USER_ROLE_CHOICES = [
+        (ADMIN_ROLE, 'Admin'),
+        (USER_ROLE, 'User'),
+    ]
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    inviter = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="membership_invites",
+        default=None,
+        blank=True,
+        null=True
+    )
+    user_role = models.CharField(max_length=5, choices=USER_ROLE_CHOICES, default=USER_ROLE)
+    confirmed = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('project', 'user',)
 
 
 class Task(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=256, blank=False, null=False)
     description = models.TextField(default='', blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
