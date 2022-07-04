26f4290a39cf9b90ce4395138dc864865e1a75ab
Morteza.AmeliBidar <mortezaameli1372@yahoo.com>
2022-07-04 17:07:16 +0430

    Change user model and set my custom user model
    (database must be delete and make migration again),
    new user model includes email filed as required filed

--- backend/config_app/settings.py
+++ backend/config_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #default app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # my app
     'rest_framework',
     'taskmanager_app',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.CustomUser'
--- backend/taskmanager_app/serializers.py
+++ backend/taskmanager_app/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Project, Task, Membership
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class MembershipSerializers(serializers.Serializer):
